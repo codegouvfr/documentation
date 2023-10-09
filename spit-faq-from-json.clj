3134a4c75c594c7bc774854eaa365e4b036c6b87
Bastien Guerry <bastien.guerry@code.gouv.fr>
2023-10-09 11:55:59 +0200

    Spit faq.json from index.json

---
+++ spit-faq-from-json.clj
@@ -0,0 +1,14 @@
+#!/usr/bin/env bb
+
+;; Copyright (c) DINUM, Bastien Guerry
+;; SPDX-License-Identifier: EPL-2.0
+;; License-Filename: LICENSES/LICENSE.EPL-2.0.txt
+
+(def faq
+  (->> (json/parse-string (slurp "index.json") true)
+       :contents
+       (filter #(= "Foire aux questions" (:raw-value (:properties %))))
+       first
+       :contents))
+
+(spit "faq.json" (json/generate-string faq))
