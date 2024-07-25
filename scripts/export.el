b5af17dfb707c3fa460026248d0d4029c01805cb
Bastien Guerry <bastien.guerry@code.gouv.fr>
2024-07-25 10:28:36 +0200

    scripts/export.el: Fail silently with broken links

--- scripts/export.el
+++ scripts/export.el
@@ -10,7 +10,7 @@
 (setq make-backup-files nil
       debug-on-error t
       org-confirm-babel-evaluate nil
-      org-export-with-broken-links 'mark)
+      org-export-with-broken-links t)
 
 ;; https://gist.github.com/zzamboni/2e6ac3c4f577249d98efb224d9d34488
 (defun org-multi-file-md-export ()
